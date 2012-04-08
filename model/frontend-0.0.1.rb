75411a9c5e5c2cc6f02e9ec0205c08bc473a7ae2
Brian McCallister <brianm@skife.org>
2012-04-08 16:02:19 -0600

    minor change

--- model/frontend-0.0.1.rb
+++ model/frontend-0.0.1.rb
@@ -5,7 +5,7 @@ system "frontend" do
 
   server "cat", base: "appserver",
   				cardinality: ["Tiger", "Fluffy"],
-    			install: ["galaxy:frontend-0.0.2-SNAPSHOT-3", 
+    			install: ["galaxy:frontend-0.0.2-SNAPSHOT2", 
     				      "lb-add:giftudi"]
 end
 
