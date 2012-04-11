7e8f8ec3f17e7afc4e2641743542a7f09819899a
Brian McCallister <brianm@skife.org>
2012-04-11 12:44:56 -0400

    change lb name

--- model/frontend.rb
+++ model/frontend.rb
@@ -1,10 +1,10 @@
 
 system "frontend" do
 
-  server "load-balancer", base: "load-balancer:giftudi?from=80&to=8000"
+  server "load-balancer", base: "load-balancer:front?from=80&to=8000"
 
   server "front", base: "appserver",
                   cardinality: ["tiger", "fluffy"],
                   install: ["galaxy:frontend-0.0.1", 
-                            "lb-add:giftudi"]
+                            "lb-add:front"]
 end
