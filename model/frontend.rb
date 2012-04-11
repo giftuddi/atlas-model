8a856ace3373b775d7dabb456a34f20f91db7765
Brian McCallister <brianm@skife.org>
2012-04-11 10:31:14 -0400

    downcase names for sanity

--- model/frontend.rb
+++ model/frontend.rb
@@ -3,8 +3,8 @@ system "frontend" do
 
   server "load-balancer", base: "load-balancer:giftudi?from=80&to=8000"
 
-  server "cat", base: "appserver",
-                cardinality: ["Tiger", "Fluffy"],
-                install: ["galaxy:frontend-0.0.1", 
-                          "lb-add:giftudi"]
+  server "front", base: "appserver",
+                  cardinality: ["tiger", "fluffy"],
+                  install: ["galaxy:frontend-0.0.1", 
+                            "lb-add:giftudi"]
 end
