e7441df93b8bdcec94301e596f7ca040c9deda00
Brian McCallister <brianm@skife.org>
2012-04-11 13:00:45 -0400

    reset

--- model/giftudi.rb
+++ model/giftudi.rb
@@ -1,4 +1,8 @@
+
 system "giftudi" do
-  system "front", external: "http://static.giftudi.com/frontend-0.0.1.rb"
-  system "back",  external: "http://static.giftudi.com/backend-0.0.1.rb"
+  system "front", 
+         external: "http://static.giftudi.com/frontend-0.0.1.rb"
+
+  system "back",  
+         external: "http://static.giftudi.com/backend-0.0.1.rb"
 end
