d95508b8053140b15d2d6bef06441ec47ca44a1e
Brian McCallister <brianm@skife.org>
2012-04-11 12:39:29 -0400

    everything model

--- model/giftudi.rb
+++ model/giftudi.rb
@@ -1,4 +1,4 @@
-system "giftudi"
+system "giftudi" do
   system "front", external: "http://static.giftudi.com/frontend-0.0.1.rb"
   system "back",  external: "http://static.giftudi.com/backend-0.0.1.rb"
 end
